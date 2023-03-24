b7700ad0820e4dbf281bdd31c996a7e8c68073b5
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-03-24 13:34:48 +0530

    Calculate Daily Employee Wage

--- src/com/bridglapz/employeewage/EmployeeWageComputation.java
+++ src/com/bridglapz/employeewage/EmployeeWageComputation.java
@@ -1,14 +1,20 @@
 package com.bridglapz.employeewage;
 
-public class EmployeeWageComputation {
+public class EmployeeWageComputation{
 	
-  public static void main(String args[]) {
-	  int Check_Employee=1;
-	  	 double Attendance_Check=Math.floor(Math.random()*10)%2;
-	  	 if(Check_Employee==Attendance_Check) {
-	  		 System.out.println("Employe Are Parsent ");
-	  	 }else {
-	  		 System.out.println("Employe Are Absent");
-	  	 }
-  }
+	public static void main(String args[]) {
+		int is_Full_Time_Employee = 1;
+		int working_Hour = 20;
+		int employe_Hour = 0;
+		int employe_Wage = 0;
+
+		double check = Math.floor(Math.random() * 10) % 2;
+		if (check == is_Full_Time_Employee) {
+			employe_Hour = 8;
+		} else {
+			employe_Hour = 0;
+		}
+		int calculate_Result = working_Hour * employe_Hour;
+		System.out.println(calculate_Result);
+	}
 }
