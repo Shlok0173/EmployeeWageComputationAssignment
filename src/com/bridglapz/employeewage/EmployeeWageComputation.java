5cc2e17dd8303a20fe6b0970dde96b43f0eca9d8
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-03-24 13:14:35 +0530

    Welcome To Employee Wage

---
+++ src/com/bridglapz/employeewage/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.bridglapz.employeewage;
+
+public class EmployeeWageComputation {
+	
+  public static void main(String args[]) {
+	  System.out.println("Welcome To Employee Wage Computaion");
+  }
+}
