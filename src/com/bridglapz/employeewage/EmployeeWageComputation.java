8bfc12c13e30860847ba3772b9bdeacca4e043aa
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-03-24 13:43:04 +0530

    added:Part Time Employee

--- src/com/bridglapz/employeewage/EmployeeWageComputation.java
+++ src/com/bridglapz/employeewage/EmployeeWageComputation.java
@@ -3,18 +3,21 @@ package com.bridglapz.employeewage;
 public class EmployeeWageComputation{
 	
 	public static void main(String args[]) {
-		int is_Full_Time_Employee = 1;
-		int working_Hour = 20;
-		int employe_Hour = 0;
-		int employe_Wage = 0;
-
-		double check = Math.floor(Math.random() * 10) % 2;
-		if (check == is_Full_Time_Employee) {
-			employe_Hour = 8;
-		} else {
-			employe_Hour = 0;
-		}
-		int calculate_Result = working_Hour * employe_Hour;
-		System.out.println(calculate_Result);
-	}
+	 	int is_Employee_Part_Time=1;
+    	int is_Employee_Full_Time=2;
+    	int Time_Work=20;
+    	int employe_Hour=0;
+    	
+    	double check=Math.floor(Math.random()*10)%3;
+    	
+    	if(check== is_Employee_Part_Time) {
+    		employe_Hour=4;
+    	}else if(check==is_Employee_Full_Time){
+    		employe_Hour=8;
+    	}else {
+    		employe_Hour=0;
+    	}
+    	int wage_Result=employe_Hour*Time_Work;
+    	System.out.println(wage_Result);
+    }
 }
