912943a4a3958b144106f0776ff0e6536dfaeef4
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-03-24 13:52:20 +0530

    Added:Switch Case

--- src/com/bridglapz/employeewage/EmployeeWageComputation.java
+++ src/com/bridglapz/employeewage/EmployeeWageComputation.java
@@ -2,22 +2,30 @@ package com.bridglapz.employeewage;
 
 public class EmployeeWageComputation{
 	
+	  public static final int is_Full_Time=2;
+	  public static final int is_Work_fee=20;
+	   public static final int is_Part_Time=1;
+	 
 	public static void main(String args[]) {
-	 	int is_Employee_Part_Time=1;
-    	int is_Employee_Full_Time=2;
-    	int Time_Work=20;
-    	int employe_Hour=0;
-    	
-    	double check=Math.floor(Math.random()*10)%3;
-    	
-    	if(check== is_Employee_Part_Time) {
-    		employe_Hour=4;
-    	}else if(check==is_Employee_Full_Time){
-    		employe_Hour=8;
-    	}else {
-    		employe_Hour=0;
-    	}
-    	int wage_Result=employe_Hour*Time_Work;
-    	System.out.println(wage_Result);
-    }
+
+	int employe_Hour=0;
+
+
+	int check=(int)Math.floor(Math.random()*10)%3;
+	switch(check) {
+	case is_Part_Time:
+		employe_Hour=4;
+		break;
+		
+	case is_Full_Time:
+		employe_Hour=8;
+		break;
+		
+	   default:
+		   employe_Hour=0;
+	}
+
+	int employeWage=employe_Hour*is_Work_fee;
+	System.out.println(employeWage);
+}
 }
