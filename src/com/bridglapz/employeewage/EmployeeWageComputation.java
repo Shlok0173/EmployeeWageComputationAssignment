6f6ce3ccebbad0943afc9bf01e688d78938e8257
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-03-24 13:21:48 +0530

    Added:Check Employee Parsent Or Absent

--- src/com/bridglapz/employeewage/EmployeeWageComputation.java
+++ src/com/bridglapz/employeewage/EmployeeWageComputation.java
@@ -3,6 +3,12 @@ package com.bridglapz.employeewage;
 public class EmployeeWageComputation {
 	
   public static void main(String args[]) {
-	  System.out.println("Welcome To Employee Wage Computaion");
+	  int Check_Employee=1;
+	  	 double Attendance_Check=Math.floor(Math.random()*10)%2;
+	  	 if(Check_Employee==Attendance_Check) {
+	  		 System.out.println("Employe Are Parsent ");
+	  	 }else {
+	  		 System.out.println("Employe Are Absent");
+	  	 }
   }
 }
