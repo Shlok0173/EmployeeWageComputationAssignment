a98432ef366394f4782e79bacf2c0be941431910
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-03-24 13:59:23 +0530

    Added:Calculating Wage for a month

--- src/com/bridglapz/employeewage/EmployeeWageComputation.java
+++ src/com/bridglapz/employeewage/EmployeeWageComputation.java
@@ -2,22 +2,36 @@ package com.bridglapz.employeewage;
 
 public class EmployeeWageComputation{
 	
-	public static void main(String args[]) {
-	 	int is_Employee_Part_Time=1;
-    	int is_Employee_Full_Time=2;
-    	int Time_Work=20;
-    	int employe_Hour=0;
-    	
-    	double check=Math.floor(Math.random()*10)%3;
-    	
-    	if(check== is_Employee_Part_Time) {
-    		employe_Hour=4;
-    	}else if(check==is_Employee_Full_Time){
-    		employe_Hour=8;
-    	}else {
-    		employe_Hour=0;
-    	}
-    	int wage_Result=employe_Hour*Time_Work;
-    	System.out.println(wage_Result);
-    }
+	
+	  public static final int is_Part_Time=1;
+	   public static final int is_Full_Time=2;
+	   public static final int is_Total_Emp_Rate=20;
+	   public static final int number_Of_Working=20;
+	   
+	   
+	   public static void main(String args[]) {
+		   int employee_Hour=0;
+		   int employee_Wage=0;
+		   int  totalEmployeeWage=0;
+		   int check=(int) Math.floor(Math.random()*10)%3;
+		   for(int day=0;day<number_Of_Working;day++) {
+		   switch(check) {
+		   case  is_Part_Time:
+			   employee_Hour=4;
+			   break;
+			   
+		   case is_Full_Time:
+			   employee_Hour=8;
+			   break;
+			   
+			default:
+				employee_Hour=0;
+		   }
+		  employee_Wage=employee_Hour* is_Total_Emp_Rate;
+		  totalEmployeeWage=totalEmployeeWage+employee_Wage;
+		  System.out.println(employee_Wage);
+		  System.out.println(totalEmployeeWage);
+	   }
+	   }
 }
+	
