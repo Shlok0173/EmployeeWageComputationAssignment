f8c25ef2f0bb48bb964d4e59401c6cb17b30ac2b
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-03-30 14:38:42 +0530

    Using Class Method

--- src/com/bridglapz/employeewage/EmployeeWageComputation.java
+++ src/com/bridglapz/employeewage/EmployeeWageComputation.java
@@ -1,38 +1,52 @@
 package com.bridglapz.employeewage;
 
-public class EmployeeWageComputation{
-	public static final int is_Part_Time_Working = 1;
-	public static final int is_Full_Time_Working = 2;
-	public static final int is_Employee_Rate_Per_Hour = 20;
-	public static final int is_Maximum_Working_Hour_Month = 100;
-	public static final int is_Maximum_Working_Day_Month = 20;
+public class EmployeeWageComputation {
 
-	public static void main(String args[]){
-		int employeeHour = 0;
-		int employeeTotalHour = 0;
-		int employeeTotalDayMonth = 0;
-		while (employeeTotalHour <= is_Maximum_Working_Hour_Month
-				&& employeeTotalDayMonth < is_Maximum_Working_Day_Month) {
-			employeeTotalDayMonth++;
+	public static final int is_part_time = 1;
+	public static final int is_full_time = 2;
+	public static final int emp_rate_per_hour = 20;
+	public static final int num_of_working_day = 20;
+	public static final int max_hours_in_month = 100;
 
-			int check = (int) Math.floor(Math.random() * 10) % 3;
-			switch (check) {
-			case is_Part_Time_Working:
-				employeeHour = 4;
+	public static void employeeWageComputation() {
+
+		System.out.println("Welcome To  Employee Wage Computaion");
+
+		int empHours = 0;
+		int totalEmpHours = 0;
+		int totalWorkingDay = 0;
+
+		int checkEmployee = (int) Math.floor(Math.random() * 10) % 3;
+
+		while (totalEmpHours <= max_hours_in_month && totalWorkingDay < num_of_working_day) {
+
+			totalWorkingDay++;
+
+			switch (checkEmployee) {
+			case is_part_time:
+				empHours = 4;
 				break;
 
-			case is_Full_Time_Working:
-				employeeHour = 8;
+			case is_full_time:
+				empHours = 8;
 				break;
 
 			default:
-				employeeHour = 0;
+				empHours = 0;
+				break;
 			}
 
-			int employeWage = employeeHour * is_Employee_Rate_Per_Hour;
-			employeeTotalHour = employeeTotalHour + employeWage;
-			System.out.println(employeWage);
-			System.out.println(employeeTotalHour);
+			int EmployeeWage = empHours * emp_rate_per_hour;
+			totalEmpHours = totalEmpHours + EmployeeWage;
+
+			System.out.println("EmployeeWage" + " " + EmployeeWage);
+			System.out.println("TotalEmpHours" + " " + totalEmpHours);
 		}
-}
+	}
+
+	public static void main(String args[]) {
+		employeeWageComputation();
+
+	}
+
 }
